bf5105591a1e275a889280881e2309470d293f25
ttndev <nghiatt@qasoftsolution.com>
2024-06-30 14:16:33 +0700

    resume-check

--- src/main/java/sushine_group/hrm_management_system/HRMManagementApplication.java
+++ src/main/java/sushine_group/hrm_management_system/HRMManagementApplication.java
@@ -2,6 +2,9 @@ package sushine_group.hrm_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HRMManagementApplication {
@@ -10,4 +13,8 @@ public class HRMManagementApplication {
 		SpringApplication.run(HRMManagementApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate customRestTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
